4ba34648d087a1fd5a8e13f0de59f41407037c63
pasalaharsha21 <98939745+pasalaharsha21@users.noreply.github.com>
2022-02-03 16:23:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let a=document.getElementById("amount").value;
+	let b=document.getElementById("myRange").value;
+	let c=document.getElementById("sel").value;
+	if(a>0){
+	let d=(a*b*c)/100;
+	let e=parseInt(c)+2020;
+	document.getElementById("simple").innerHTML="If you deposit".concat(" ",a)+","+"<br>"+"at an interest rate of".concat(" ",b)+"%"+"."+"<br>"+"you will recieve an amount".concat(" ",d)+","+"<br>"+"in the year of".concat(" ",e);
+	}
+	else{
+		alert("enter the positive number");
+	}
 }
-        
